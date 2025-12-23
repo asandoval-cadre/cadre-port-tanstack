a3c6f88d2ae53cfec5bc05e4c9493c64233ef23a
Adrian Sandoval <adrian.sandoval@gocadre.ai>
2025-12-23 12:51:44 -0800

    create dashboard layout

---
+++ src/routes/_authenticated/_dashboard.tsx
@@ -0,0 +1,25 @@
+import { Outlet, createFileRoute } from '@tanstack/react-router'
+
+import { DashboardSidebar } from '@/components/dashboard-sidebar'
+import { DashboardTopbar } from '@/components/dashboard-topbar'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+
+export const Route = createFileRoute('/_authenticated/_dashboard')({
+    component: RouteComponent
+})
+
+const sidebarItems = [{ title: 'Dashboard', to: '/dashboard' }]
+
+function RouteComponent() {
+    return (
+        <SidebarProvider>
+            <DashboardTopbar />
+            <div className="flex min-h-screen w-full pt-16">
+                <DashboardSidebar items={sidebarItems} />
+                <SidebarInset className="bg-stone-50 p-8">
+                    <Outlet />
+                </SidebarInset>
+            </div>
+        </SidebarProvider>
+    )
+}
